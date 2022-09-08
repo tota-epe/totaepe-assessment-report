b859cb5f063d3b37db4bad30089bf5ce4bf2f96d
Reinaldo Mello <rmello@gmail.com>
2022-09-08 14:36:29 -0300

    Adds % of success on each attempt

--- common/components/word/word.tsx
+++ common/components/word/word.tsx
@@ -20,6 +20,7 @@ export class Word extends React.Component<WordProps, WordState> {
   withError: number[] = []
   withConceptError: number[] = []
   destinationNodeID?: string = ''
+  rawErrorCount: JSX.Element[] = []
 
   constructor(props: WordProps) {
     super(props)
@@ -52,6 +53,7 @@ export class Word extends React.Component<WordProps, WordState> {
       withError: [0, 0],
       withConceptError: [0, 0],
       letterData: this.word.split('').map(letter => { return { } as ErrorProfile }),
+      rawErrorCount: [] as number[], // Marks total count of error on each letter
       chartData: []
     }
 
@@ -102,9 +104,17 @@ export class Word extends React.Component<WordProps, WordState> {
             }
             r[index][error].count += 1
             r[index][error].occurrences = r[index][error].occurrences.concat(errorsOnLetter[error].occurrences)
+            break; // Consider only the first error on each letter & interaction
           }
         })
       }
+      statement.response.forEach(element => {
+        let rawErrorCount = element.length - 1;
+        if (rawErrorCount > 2) {
+          rawErrorCount = 3;
+        }
+        reducedStatements.rawErrorCount[rawErrorCount] = (reducedStatements.rawErrorCount[rawErrorCount] || 0) + 1
+      });
       return reducedStatements
     }), reducedStatements)
 
@@ -112,6 +122,10 @@ export class Word extends React.Component<WordProps, WordState> {
     this.withError = reducedStatements.withError
     this.withConceptError = reducedStatements.withConceptError
     this.correct = reducedStatements.correct
+    const totalAttempts = reducedStatements.rawErrorCount.reduce((s, a) => s + a, 0);
+    this.rawErrorCount = reducedStatements.rawErrorCount.map((e, index) => {
+      return this.numberWithPercent(e, totalAttempts)
+    })
   }
 
   toggleStatements() {
@@ -160,6 +174,12 @@ export class Word extends React.Component<WordProps, WordState> {
       <div>
         <h4>{this.word} - grupo conceito: {this.word.slice(this.conceptRange[0], this.conceptRange[1] + 1)}</h4>
         <p>Media Móvel 5 ultimas {this.statements[0]?.ma5?.toLocaleString(undefined, { style: 'percent', minimumFractionDigits: 2 })}</p>
+        <p>Perfil de acertos pelo total de letras digitadas:
+        { this.rawErrorCount[0] && (<p>Sem erros: {this.rawErrorCount[0]}</p>) }
+        { this.rawErrorCount[1] && (<p>Segunda tentativa: {this.rawErrorCount[1]}</p>) }
+        { this.rawErrorCount[2] && (<p>Terceira tentativa: {this.rawErrorCount[2]}</p>) }
+        { this.rawErrorCount[3] && (<p>Com 3+ tentativas: {this.rawErrorCount[3]}</p>) }
+        </p>
         <table>
           <thead>
             <tr><td></td><td>Total</td><td>Mais recentes</td><td>Ultimas 24hs</td></tr>
