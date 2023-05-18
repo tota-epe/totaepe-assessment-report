9527beaa9e44583bb3e9c3c419b685b70c84fb86
Reinaldo Mello <rmello@gmail.com>
2023-05-18 10:17:17 -0300

    Disable links on users wuth no data

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,20 +3,26 @@ import Link from "next/link";
 
 import connectToDatabase from "../utils/db";
 import { StudentModel, Student } from "../models/student";
+import { getLRSPeople } from "../modules/lrs/people";
 
 const Home: NextPage = ({
   students,
+  people,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <>
       <h1>Alunos</h1>
       <ul>
         {students.map((student: Student) => {
+          const hasData = people.find((p: string) => p === student.accountName);
           return (
             <li key={student.code}>
-              <Link href={`/person/${student.accountName}`}>
-                {student.name}
-              </Link>
+              {hasData && (
+                <Link href={`/person/${student.accountName}`}>
+                  {student.name}
+                </Link>
+              )}
+              {!hasData && <>{student.name}</>}
             </li>
           );
         })}
@@ -28,10 +34,11 @@ const Home: NextPage = ({
 // This also gets called at build time
 export const getStaticProps: GetStaticProps = async () => {
   await connectToDatabase();
+  const people = (await getLRSPeople()).map((p) => p.accountName);
   const students = await StudentModel.find();
 
   return {
-    props: { students: JSON.parse(JSON.stringify(students)) },
+    props: { students: JSON.parse(JSON.stringify(students)), people },
     revalidate: 60,
   };
 };
